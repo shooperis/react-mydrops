e86428ea663460030814b5f85f74f3c5816c5fbd
Dominykas Marcenkovas <dominykas@veiks.lt>
2023-03-13 08:15:45 +0200

    added responsive to 404 page

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -15,7 +15,6 @@ const PageNotFound = () => {
       <div className="content">
         <h1 className="title">Page not found</h1>
 
-        <br />
         The requested URL was not found on this website
         <br /><br />
         Most likely causes:
@@ -29,7 +28,7 @@ const PageNotFound = () => {
         <br />
         <ul>
           <li>Retype the address.</li>
-          <li>Go back to the previuos page.</li>
+          <li>Go back to the previous page.</li>
           <li>Go to <Link to={APP_SETTINGS.address}>{APP_SETTINGS.address}</Link></li>
         </ul>
       </div>
