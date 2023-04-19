578356b3397ec9fb7c83301dadd5085bd3e33419
Dominykas Marcenkovas <dominykas@veiks.lt>
2023-04-19 11:31:44 +0300

    added post animations with react transition group

--- src/components/PostsList/PostItem/PostItem.js
+++ src/components/PostsList/PostItem/PostItem.js
@@ -1,12 +1,42 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useState, useRef } from "react";
 import { NavLink } from "react-router-dom";
 import { API_URL } from "./../../../utils/config";
 import { prettyDate, postContentRender } from "./../../../utils/functions";
 import UserContext from "./../../../store/user-context";
 import useHttp from "../../../hooks/use-http";
+import { Transition } from "react-transition-group";
+
+const animationDuration = 250;
+
+const animationDefaultStyle = {
+  transition: `opacity ${animationDuration}ms ease-in-out, max-height ${animationDuration}ms ease-in-out, margin-bottom ${animationDuration}ms ease-in-out`,
+  opacity: 0,
+  maxHeight: "0px",
+  minHeight: "unset",
+  marginBottom: "0px",
+};
+
+const animationTransitionStyles = {
+  entering: { borderWidth: '0px' },
+  entered: {
+    opacity: 1,
+    maxHeight: "450px",
+    minHeight: "55px",
+    marginBottom: "40px",
+    borderWidth: '1px'
+  },
+  // exiting:  { opacity: 0 },
+  // exited: {
+  //   opacity: 0,
+  //   maxHeight: "0px",
+  //   minHeight: "unset",
+  //   marginBottom: "0px",
+  // },
+};
 
 const PostItem = (props) => {
-  const [animationClass, setAnimationClass] = useState("");
+  const [inProp, setInProp] = useState(true);
+  const nodeRef = useRef(null);
   const userCtx = useContext(UserContext);
   const fetchData = useHttp();
 
@@ -26,66 +56,66 @@ const PostItem = (props) => {
     postTypeClass = "unknown";
   }
 
-  const onDeletePost = async () => {
-    if (animationClass !== "hidden") {
-      setAnimationClass("hidden");
-
-      setTimeout(async () => {
-        await fetchData({
-          url: `${API_URL}/posts/${id}`,
-          method: "DELETE",
-          actionOrigin: "DeletingPost",
-        });
-
-        userCtx.deletePost(id);
-      }, 250);
-    }
+  const onHidingPost = () => {
+    setInProp(false);
   };
 
-  useEffect(() => {
-    if (userCtx.indicator.status === "POST_CREATED") {
-      if (id === userCtx.indicator.data) {
-        setAnimationClass("hidden");
+  const onDeletePost = async () => {
+    await fetchData({
+      url: `${API_URL}/posts/${id}`,
+      method: "DELETE",
+      actionOrigin: "DeletingPost",
+    });
 
-        const timer = setTimeout(() => {
-          setAnimationClass("");
-          userCtx.clearIndicator();
-        }, 10);
-
-        return () => {
-          clearTimeout(timer);
-        };
-      }
-    }
-  }, []);
+    userCtx.deletePost(id);
+  };
 
   return (
-    <div className={`post-item ${animationClass}`}>
-      <div className="detail">
-        <div className="type">{type}</div>
-        <div className="date">{prettyDate(props.createdDate)}</div>
-      </div>
-      <div className="control-wrapper">
-        <div className={"control" + (postTypeClass ? ` ${postTypeClass}` : "")}>
-          <NavLink
-            className="open-post"
-            to={`post/${props.keyId}`}
-            title="Open this post"
-          >
-            Open this post
-          </NavLink>
-          <button
-            className="delete-post"
-            title="Delete this post"
-            onClick={onDeletePost}
-          >
-            Delete this post
-          </button>
-          <i className="icon"></i>
+    <Transition
+      nodeRef={nodeRef}
+      in={inProp}
+      appear={true}
+      timeout={animationDuration}
+      onExited={onDeletePost}
+    >
+      {(state) => (
+        <div
+          className="post-item"
+          ref={nodeRef}
+          style={{
+            ...animationDefaultStyle,
+            ...animationTransitionStyles[state],
+          }}
+        >
+          <div className="detail">
+            <div className="type">{type}</div>
+            <div className="date">{prettyDate(props.createdDate)}</div>
+          </div>
+          <div className="control-wrapper">
+            <div
+              className={"control" + (postTypeClass ? ` ${postTypeClass}` : "")}
+            >
+              <NavLink
+                className="open-post"
+                to={`post/${props.keyId}`}
+                title="Open this post"
+              >
+                Open this post
+              </NavLink>
+              <button
+                className="delete-post"
+                title="Delete this post"
+                onClick={onHidingPost}
+              >
+                Delete this post
+              </button>
+              <i className="icon"></i>
+            </div>
+          </div>
+          {postContentRender(props.content, type)}
         </div>
-      </div>
-      {postContentRender(props.content, type)}
-    </div>
+      )}
+    </Transition>
   );
 };
 
