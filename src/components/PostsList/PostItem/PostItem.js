a16fca4c093f9858c39144268c76db7ac0b2f0a7
Dominykas Marcenkovas <dominykas@veiks.lt>
2023-04-19 21:00:20 +0300

    changed post animations to TransitionGroup (auto mounting, unmounting)

--- src/components/PostsList/PostItem/PostItem.js
+++ src/components/PostsList/PostItem/PostItem.js
@@ -1,42 +1,11 @@
-import { useContext, useState, useRef } from "react";
+import { useContext } from "react";
 import { NavLink } from "react-router-dom";
 import { API_URL } from "./../../../utils/config";
 import { prettyDate, postContentRender } from "./../../../utils/functions";
 import UserContext from "./../../../store/user-context";
 import useHttp from "../../../hooks/use-http";
-import { Transition } from "react-transition-group";
-
-const animationDuration = 250;
-
-const animationDefaultStyle = {
-  transition: `opacity ${animationDuration}ms ease-in-out, max-height ${animationDuration}ms ease-in-out, margin-bottom ${animationDuration}ms ease-in-out`,
-  opacity: 0,
-  maxHeight: "0px",
-  minHeight: "unset",
-  marginBottom: "0px",
-};
-
-const animationTransitionStyles = {
-  entering: { borderWidth: '0px' },
-  entered: {
-    opacity: 1,
-    maxHeight: "450px",
-    minHeight: "55px",
-    marginBottom: "40px",
-    borderWidth: '1px'
-  },
-  // exiting:  { opacity: 0 },
-  // exited: {
-  //   opacity: 0,
-  //   maxHeight: "0px",
-  //   minHeight: "unset",
-  //   marginBottom: "0px",
-  // },
-};
 
 const PostItem = (props) => {
-  const [inProp, setInProp] = useState(true);
-  const nodeRef = useRef(null);
   const userCtx = useContext(UserContext);
   const fetchData = useHttp();
 
@@ -56,66 +25,42 @@ const PostItem = (props) => {
     postTypeClass = "unknown";
   }
 
-  const onHidingPost = () => {
-    setInProp(false);
-  };
-
   const onDeletePost = async () => {
     await fetchData({
       url: `${API_URL}/posts/${id}`,
       method: "DELETE",
       actionOrigin: "DeletingPost",
     });
-
     userCtx.deletePost(id);
   };
 
   return (
-    <Transition
-      nodeRef={nodeRef}
-      in={inProp}
-      appear={true}
-      timeout={animationDuration}
-      onExited={onDeletePost}
-    >
-      {(state) => (
-        <div
-          className="post-item"
-          ref={nodeRef}
-          style={{
-            ...animationDefaultStyle,
-            ...animationTransitionStyles[state],
-          }}
-        >
-          <div className="detail">
-            <div className="type">{type}</div>
-            <div className="date">{prettyDate(props.createdDate)}</div>
-          </div>
-          <div className="control-wrapper">
-            <div
-              className={"control" + (postTypeClass ? ` ${postTypeClass}` : "")}
-            >
-              <NavLink
-                className="open-post"
-                to={`post/${props.keyId}`}
-                title="Open this post"
-              >
-                Open this post
-              </NavLink>
-              <button
-                className="delete-post"
-                title="Delete this post"
-                onClick={onHidingPost}
-              >
-                Delete this post
-              </button>
-              <i className="icon"></i>
-            </div>
-          </div>
-          {postContentRender(props.content, type)}
+    <div className="post-item" ref={props.nodeRef}>
+      <div className="detail">
+        <div className="type">{type}</div>
+        <div className="date">{prettyDate(props.createdDate)}</div>
+      </div>
+      <div className="control-wrapper">
+        <div className={"control" + (postTypeClass ? ` ${postTypeClass}` : "")}>
+          <NavLink
+            className="open-post"
+            to={`post/${props.keyId}`}
+            title="Open this post"
+          >
+            Open this post
+          </NavLink>
+          <button
+            className="delete-post"
+            title="Delete this post"
+            onClick={onDeletePost}
+          >
+            Delete this post
+          </button>
+          <i className="icon"></i>
         </div>
-      )}
-    </Transition>
+      </div>
+      {postContentRender(props.content, type)}
+    </div>
   );
 };
 
--- src/components/PostsList/PostsList.js
+++ src/components/PostsList/PostsList.js
@@ -1,5 +1,6 @@
-import { useContext } from "react";
+import { useContext, createRef } from "react";
 import UserContext from "../../store/user-context";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 import PostItem from "./PostItem/PostItem";
 import NewbieTour from "../NewbieTour/NewbieTour";
 
@@ -9,23 +10,40 @@ const PostsList = () => {
   const userCtx = useContext(UserContext);
 
   const renderList = () => {
-    return userCtx.posts.map((post) => (
-      <PostItem
-        key={post.id}
-        id={post.id}
-        keyId={post.key}
-        content={post.type === "youtube" || post.type === "vimeo" ? post.additionalData : post.content}
-        type={post.type}
-        createdDate={post.createdDate}
-      />
-    ));
+    return userCtx.posts.map((post) => {
+      const nodeRef = createRef(null);
+
+      return (
+        <CSSTransition
+          key={post.id}
+          nodeRef={nodeRef}
+          appear={true}
+          timeout={250}
+        >
+          <PostItem
+            id={post.id}
+            keyId={post.key}
+            content={
+              post.type === "youtube" || post.type === "vimeo"
+                ? post.additionalData
+                : post.content
+            }
+            type={post.type}
+            createdDate={post.createdDate}
+            nodeRef={nodeRef}
+          />
+        </CSSTransition>
+      );
+    });
   };
 
   if (userCtx.posts.length < 1) {
     return <NewbieTour />;
   }
 
-  return <div className="posts-list">{renderList()}</div>;
+  return (
+    <TransitionGroup className="posts-list">{renderList()}</TransitionGroup>
+  );
 };
 
 export default PostsList;
